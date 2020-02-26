9e260360c5406c669620bb1917cd862832bffdc1
Helen Cho <helencho2014@gmail.com>
2020-02-26 15:25:23 -0500

    actually fixed?

--- lab7_part2.ml
+++ lab7_part2.ml
@@ -82,9 +82,10 @@ Exercise 2A: Replace the `0` in the expression below with an
 expression that extracts the red channel of the color named Red,
 thereby naming the result `red_channel`.
 ......................................................................*)
-open Color ;;
 
-let red_channel = red ;;
+let red_channel = 
+  let open Color in 
+  red (color_named Red) ;;
 
 (* Let's investigate one way that a signature can be useful. Although
 color.ml contains an implementation of a basic color module, the
