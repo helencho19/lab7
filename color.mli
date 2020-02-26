8d1eb45a2cfcedbf5db400bd7949563daa80b8ec
Helen Cho <helencho2014@gmail.com>
2020-02-26 15:13:07 -0500

    color.mli

---
+++ color.mli
@@ -0,0 +1,17 @@
+module type COLOR = 
+  sig 
+    type color = int
+    type color_name =
+    | Red
+    | Green
+    | Blue
+    | Orange
+    | Yellow
+    | Indigo
+    | Violet 
+    val to_color : int -> int -> int -> color 
+    val red : color -> int 
+    val green : color -> int
+    val blue : color -> int 
+    val color_name : color_name -> color 
+end ;;
